f185e551e5cb09e64f4450f4710c1c88a49c5cf0
Mikael1287 <mikael747@gmail.com>
2022-10-08 22:44:59 -0400

    commit update

--- project/src/main/java/com/example/project/ProjectApplication.java
+++ project/src/main/java/com/example/project/ProjectApplication.java
@@ -2,12 +2,20 @@ package com.example.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@RestController
+
 public class ProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(ProjectApplication.class, args);
+}
 
+@GetMapping("/hello")
+public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {//// designed to take a String value called "name" and then combines with "H ello", if no arg is passed default value will be "World"
+return String.format("Hello %s!", name);
+}
 }
